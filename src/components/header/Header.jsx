59fe6e74d8512d5441c077b471044c3d73cbf75e
Ralphael oshun <ralphaeloshun@Afo-Lappy.local>
2022-08-17 23:48:04 +0100

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assests/modelPortfolio.png'
 import './header.css'
+import HeaderScroll from './HeaderScroll'
 const Header = () => {
   return (
-    <div>Header</div>
+  <header>
+    <div className="container header__container">
+<h5> Hello I'm</h5>
+<h1>Rafa Oshun</h1>
+
+<h5 className="text-light">
+FullStack Developer
+</h5>
+<CTA/>
+<HeaderScroll/>
+<div className="me">
+  <img src ={ME} alt="" />
+</div>
+<a href='#contact' className='scroll__down'>Scroll Down</a>
+    </div>
+  </header>
   )
 }
 
