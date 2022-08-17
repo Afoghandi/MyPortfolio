928d4b08a2c36d17cdc6e5f66c369786439abb45
Ralphael oshun <ralphaeloshun@Afo-Lappy.local>
2022-08-17 19:30:34 +0100

    setting the index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 
 const App = () => {
   return (
-    <div>App</div>
+<>
+<Header/>
+<Navbar/>
+<About/>
+
+<Experience/>
+<Services/>
+<Portfolio/>
+<Testimonials/>
+<Contact/>
+<Footer/>
+
+</>
+
+    
   )
 }
 
