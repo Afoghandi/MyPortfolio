05ab3ee69b9ab8e5fbed34540f4f4e74b1c417f7
Ralphael oshun <ralphaeloshun@Afo-Lappy.local>
2022-08-19 11:59:08 +0100

    completing about page

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './experience.css'
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+<h5>What Skills I Have</h5>
+<h2> My Experience</h2>
+
+<div className="container experience__container">
+<div className="experience__frontend">
+  
+</div>
+
+</div>
+
+    </section>
   )
 }
 
