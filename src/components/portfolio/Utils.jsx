4596cc926e170503d17797f0d4938958c1fd53a2
Ralphael oshun <ralphaeloshun@Afo-Lappy.local>
2022-08-19 23:56:21 +0100

    Portfolio Component

---
+++ src/components/portfolio/Utils.jsx
@@ -0,0 +1,46 @@
+import IMG1 from '../../assests/project4.jpg'
+
+ export const data =[
+    {
+        id:1,
+        image:IMG1,
+        title:'Crypto Currency Dashboard & Finance',
+        github: 'https://github.com',
+        demo:'https://github.com'
+    },
+    {
+        id:2,
+        image:IMG1,
+        title:'Charts template and Infographic',
+        github: 'https://github.com',
+        demo:'https://github.com'
+    },
+    {
+        id:3,
+        image:IMG1,
+        title:'Figma dashboard UI Kit',
+        github: 'https://github.com',
+        demo:'https://github.com'
+    },
+    {
+        id:4,
+        image:IMG1,
+        title:'Maintaining Tasks and Tracking',
+        github: 'https://github.com',
+        demo:'https://github.com'
+    },
+    {
+        id:5,
+        image:IMG1,
+        title:'E-Commerce with Stripe payment',
+        github: 'https://github.com',
+        demo:'https://github.com'
+    },
+    {
+        id:6,
+        image:IMG1,
+        title:'Covid 19 Tracker',
+        github: 'https://github.com',
+        demo:'https://github.com'
+    }
+]
