61b4606c91b1624e924f6880a406cc91422b5a7f
Ralphael oshun <ralphaeloshun@Afo-Lappy.local>
2022-08-20 20:04:33 +0100

    Testimonials Component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
 import './testimonials.css'
+import {Data} from './Utils'
+
+// Import Swiper core required modules
+import {  Pagination, Navigation} from 'swiper'
+
+import{ Swiper, SwiperSlide} from 'swiper/react'
+
+//Import Swiper styles
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const Testimonials = () => {
   return (
-    <section id='testimonials'>testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container" 
+      modules={[ Pagination, Navigation]} 
+      spaceBetween={40} 
+      slidesPerView={1} 
+      pagination={{clickable:true}} >
+        
+
+{Data.map(({avatar, name, review}, index) =>{
+  return(
+    <SwiperSlide className="testimonial" key={index}>
+    <div className="client__avatar">
+      <img src={avatar} alt='Avatar1'/>
+     
+    </div>
+    <h5 className='client__name'>
+        {name}
+      </h5>
+      <small className='client__review'>
+
+        {review}
+      </small>
+  </SwiperSlide>
+  )
+} )}
+       
+
+       
+        
+        
+      </Swiper>
+      </section>
   )
 }
 
---
+++ src/components/testimonials/Utils.js
@@ -0,0 +1,33 @@
+import AVTR1 from '../../assests/project4.jpg'
+import AVTR2 from '../../assests/project3.jpg'
+import AVTR3 from '../../assests/project4.jpg'
+import AVTR4 from '../../assests/project4.jpg'
+
+
+
+export const Data =[
+
+{
+    avatar: AVTR1,
+    name:'Tina Sims',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, repudiandae?'
+},
+{
+    avatar: AVTR2,
+    name:'Sheta Wale',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, repudiandae?'
+},
+{
+    avatar: AVTR3,
+    name:'Kwame Pongo',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, repudiandae?'
+},
+{
+    avatar: AVTR4,
+    name:'Tunde Afolabi',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, repudiandae?'
+},
+
+
+
+]
